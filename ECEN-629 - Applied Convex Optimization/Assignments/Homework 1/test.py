f4e73137d0f016a55437f370a5a64e44c5c95fb6
Brody Jordan <bljordan4@gmail.com>
2026-01-01 15:12:14 -0600

    Archived old courses

--- ECEN-629 - Applied Convex Optimization/Assignments/Homework 1/test.py
+++
@@ -1,5 +0,0 @@
-from pdfminer.high_level import extract_text
-from pathlib import Path
-
-pdf_path = Path(r"c:\Users\bljor\Sync\Courses\ECEN-629 - Applied Convex Optimization\Assignments\Homework 1\HW1_TAMU_2025.pdf")
-print(extract_text(str(pdf_path)))
